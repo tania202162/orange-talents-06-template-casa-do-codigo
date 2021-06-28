6ea5c84552c4a0837e5a68ab55b74756d172f729
unknown <tania.alves@zup.com.br>
2021-06-28 09:54:01 -0300

    setup do projeto

---
+++ src/main/java/br/com/zup/academy/tania/casodocodigo/CasoDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.tania.casodocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoDoCodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/academy/tania/casodocodigo/CasoDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.tania.casodocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
